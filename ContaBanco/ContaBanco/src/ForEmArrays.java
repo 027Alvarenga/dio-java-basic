d76933c603da0abbe91406e147bcefa3f1e5246f
Jose Marcio <josemarciojunior10@gmail.com>
2024-08-04 10:03:24 -0300

    commit for em arrays

---
+++ ContaBanco/ContaBanco/src/ForEmArrays.java
@@ -0,0 +1,13 @@
+public class ForEmArrays {
+
+    public static void main(String[] args) {
+        
+        String alunos[] = {"FELIPE", "JONAS", "JULIA", "MARCOS"};
+
+        for (int i = 0; i < alunos.length; i++) {
+            System.out.println("O aluno no indice i=" + i + " e " + alunos[i]);
+        }
+
+
+    }
+}
