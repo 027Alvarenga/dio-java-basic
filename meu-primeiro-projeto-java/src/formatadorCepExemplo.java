ef29b957a1774d901296f7488c348ca9eaf34888
Jose Marcio <josemarciojunior10@gmail.com>
2024-08-05 20:38:12 -0300

    Atualizações dos códigos e inserção dos códigos de estruturas excepcionais

---
+++ meu-primeiro-projeto-java/src/formatadorCepExemplo.java
@@ -0,0 +1,21 @@
+
+import com.sun.source.doctree.SystemPropertyTree;
+
+public class formatadorCepExemplo {
+    public static void main(String[] args) {
+        try {
+            String cepFormatado = formatarCep("23.765-064");
+            System.out.println(cepFormatado);
+        } catch (CepInvalidoException e) {
+            System.out.println("O CEP nao corresponde com as regras de negocio");
+        }
+    }
+
+    static String formatarCep(String cep) throws CepInvalidoException{
+        if(cep.length() != 8)
+          throw new CepInvalidoException();
+        
+          //simulando um cep formatado
+          return "23.765-064";
+    }
+}
